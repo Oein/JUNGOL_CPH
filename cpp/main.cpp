e92524b985812f07a5b3035a20df0be2013c5af1
Oein <oein0219@gmail.com>
2023-02-12 18:01:56 +0900

    Initial Commit

---
+++ cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Dev on 2023/02/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
